4aea913b04e675de56731c3aa3cd1b14ce743afc
Alexandra Collins <andracolexa@gmail.com>
2020-02-05 16:22:23 +0100

    implement width and height properties in container widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.deepOrangeAccent,
-      child: Text(
-        "Pizza",
-        textDirection: TextDirection.ltr,
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.deepOrangeAccent,
+        width: 192.0,
+        height: 96.0,
+        // This is how to use margin in flutter => margin: EdgeInsets.all(50.0),
+        child: Text(
+          "Pizza",
+          textDirection: TextDirection.ltr,
         )
+      )
     );
   }
   
