9239d50a41a7a1250c876351d22c958c4284afe7
Alexandra Collins <andracolexa@gmail.com>
2020-02-05 17:32:43 +0100

    implement rows and column in flutter app

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,20 +7,66 @@ class Home extends StatelessWidget {
     return Center(
       child: Container(
         alignment: Alignment.center,
+        padding: EdgeInsets.only(top: 30.0, left: 10.0),
         color: Colors.deepOrangeAccent,
         // This is how you use width and height in flutter =>
         // width: 192.0,
         // height: 96.0,
         // This is how to use margin in flutter => margin: EdgeInsets.all(50.0),
-        child: Text(
-          "Pizza",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 80.0,
-            decoration: TextDecoration.none,
-            fontFamily: "Oxygen",
-            fontWeight: FontWeight.normal,
+        child: Column(children: <Widget>[
+          Row(children: <Widget>[
+          Expanded(
+            child: Text(
+            "Margherita",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 20.0,
+              decoration: TextDecoration.none,
+              fontFamily: "Oxygen",
+              fontWeight: FontWeight.normal,
+            )
+            )
+          ),
+        Expanded(
+          child: Text(
+            "Tomato, Mozzarella, Basil",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 20.0,
+              decoration: TextDecoration.none,
+              fontFamily: "Oxygen",
+              fontWeight: FontWeight.normal,
+            )
           )
+        )
+        ],),
+        Row(children: <Widget>[
+          Expanded(
+            child: Text(
+            "Marinara",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 20.0,
+              decoration: TextDecoration.none,
+              fontFamily: "Oxygen",
+              fontWeight: FontWeight.normal,
+            )
+            )
+          ),
+        Expanded(
+          child: Text(
+            "Tomato, Garlic",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 20.0,
+              decoration: TextDecoration.none,
+              fontFamily: "Oxygen",
+              fontWeight: FontWeight.normal,
+            )
+          )
+        )
+        ],)
+        ],
         )
       )
     );
