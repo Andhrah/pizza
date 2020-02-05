b022f98a7f190b9c0bd79e24cf26ce647280f4e7
Alexandra Collins <andracolexa@gmail.com>
2020-02-05 16:32:13 +0100

    remove text decoration

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,12 +8,17 @@ class Home extends StatelessWidget {
       child: Container(
         alignment: Alignment.center,
         color: Colors.deepOrangeAccent,
-        width: 192.0,
-        height: 96.0,
+        // This is how you use width and height in flutter =>
+        // width: 192.0,
+        // height: 96.0,
         // This is how to use margin in flutter => margin: EdgeInsets.all(50.0),
         child: Text(
           "Pizza",
           textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 80.0,
+            decoration: TextDecoration.none,
+          )
         )
       )
     );
